fc77bd2540517a781377085e61ea61a018347438
Rashed967 <rashedhasan967.com>
2023-04-11 22:25:05 +0600

    applied job added done

---
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -0,0 +1,45 @@
+import { faDollar, faLocation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const AppliedJob = ({job}) => {
+    const {
+        company_logo, 
+        job_title, 
+        company_name, 
+        remote_or_onsite,
+        location,
+        fulltime_or_parttime, 
+        salary, 
+        experiences,
+        id,
+
+    } = job
+    return (
+        <>
+            <div className=" p-5 card-compact my-3 bg-base-100 shadow-md text-left border flex items-center rounded-md">
+  <figure className='w-24 mx-5'><img src={company_logo} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{job_title}</h2>
+    <p>{company_name}</p>
+    <div className='inline-flex gap-5 w-1/4'>
+        <p className='text-blue-600 font-semibold text-center py-2 px-3 border border-blue-600 rounded-sm'>{remote_or_onsite}</p>
+        <p className='text-blue-600 font-semibold text-center py-2 px-3 border border-blue-600 rounded-sm'>{fulltime_or_parttime}</p>
+    </div>
+    <div className='flex items-center gap-4'>
+    <span><FontAwesomeIcon icon={faLocation} /> {location}</span>
+    <span><FontAwesomeIcon icon={faDollar} /> Salary : {salary}</span>
+    </div>
+  </div>
+
+    <Link  to={`/job-details/${id}`}>
+    <button  className='rounded-md text-white font-semibold py-3 px-4 bg-[#7E90FE]'>View Details</button>
+    </Link>
+
+</div>
+        </>
+    );
+};
+
+export default AppliedJob;
--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getShoppingCart } from '../../../utils/fakedb';
+import AppliedJob from '../AppliedJob/AppliedJob';
 
 const AppliedJobs = () => {
+    
     const getStoredCart = getShoppingCart()
-    console.log(getStoredCart)
+   
+    const [jobs, setJobs] = useState([])
+    useEffect(() => {
+        fetch('/jobs.json')
+        .then(res => res.json())
+        .then(data => setJobs(data))
+      }, [])
+      
+    const appliedJobs = []
+      for(const id in getStoredCart){  
+        jobs.filter(job => job.id === getStoredCart[id] && appliedJobs.push(job))  
+      }
+
     return (
         <div>
-            <h2>Hello from applied jobs</h2>
+            <div className='bg-emerald-500 py-14 rounded-sm'>
+                    <h3 className='text-3xl font-semibold'>Applied Job</h3>
+            </div>
+            {
+                appliedJobs.map(job => <AppliedJob
+                key={job.id}
+                job={job}
+                ></AppliedJob>)
+            }
         </div>
     );
 };
